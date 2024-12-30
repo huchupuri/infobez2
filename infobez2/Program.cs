ef0022c3d4a8624fba995abefffc9fb77322e3a7
huchupuri <squaer120@gmail.com>
2024-12-30 22:59:17 +0300

    Добавьте файлы проекта.

---
+++ infobez2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите ключ: ");
+        string key = Console.ReadLine();
+        Console.Write("Введите строку для шифрования: ");
+        string input = Console.ReadLine();
+
+        string encryptedText = Encrypt(input, key);
+
+        Console.Write("Зашифрованное сообщение: ");
+        Console.WriteLine(encryptedText);
+    }
+
+    static string Encrypt(string text, string key)
+    {
+        string alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        int alphabetLength = alphabet.Length;
+
+        text = text.ToLower();
+        key = key.ToLower();
+
+        char[] result = new char[text.Length];
+
+        for (int i = 0, j = 0; i < text.Length; i++)
+        {
+            char currentChar = text[i];
+            int charIndex = alphabet.IndexOf(currentChar);
+            if (charIndex == -1)
+            {
+                result[i] = currentChar;
+                continue;
+            }
+            int keyIndex = alphabet.IndexOf(key[j % key.Length]);
+            int encryptedIndex = (charIndex + keyIndex) % alphabetLength;
+            result[i] = alphabet[encryptedIndex];
+            j+=1; 
+        }
+
+        return new string(result);
+    }
+}
